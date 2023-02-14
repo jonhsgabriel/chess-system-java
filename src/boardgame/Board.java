dc6711cda6a629755a61a8fcc469a6eb50127226
Joao Gabriel <jgbrlmotta@gmail.com>
2023-02-14 14:00:30 -0300

    Alterções de posicionamento

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -41,6 +41,18 @@ public class Board {
 		pieces[position.getRow()][position.getColum()] = piece;
 		piece.position = position;
 	}
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não está no tabuleiro");
+		}
+		if(piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColum()] = null;
+		return aux;
+	}
 	private boolean positionExists(int row, int colum) {
 		return row >=0 && row < rows && colum >=0 && colum < colums;
 	}
